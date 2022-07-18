84e7456415f32f930345e722c1c390dbff988a02
Salt Developer <supermaher2@gmail.com>
2022-07-18 16:55:09 +0200

    change github account

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../redux/userSlice';
