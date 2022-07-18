ce9984d5c8330fb3f9333165feca8ebfb36af95e
Salt Developer <saltdev@Admins-MacBook-Pro.local>
2022-07-18 15:34:06 +0200

    Add user slice and enable devtools

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './src/components/redux/store'
+import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-  // <React.StrictMode>
+   {/* <React.StrictMode> */}
     <App />
-  // </React.StrictMode>
+   {/* </React.StrictMode> */}
   </Provider>
 );
 
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import tripReducer from "./tripSlice"
+import userReducer from "./userSlice"
 
 export const store = configureStore({
   reducer: {
-    trips: tripReducer
+    trips: tripReducer,
+    user: userReducer
   },
+  devTools: true
 })
