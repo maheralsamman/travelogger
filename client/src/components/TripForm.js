76557f16e090358a425671b0fe6f49d505d5156e
Salt Developer <supermaher2@gmail.com>
2022-07-21 10:18:08 +0200

    add logic to step back and forward through form

--- client/src/components/TripForm.js
+++ client/src/components/TripForm.js
@@ -1,103 +1,161 @@
 import ImageUpload from "./ImageUpload";
-import { useState, useEffect } from "react";
-import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
-import { TiTick } from 'react-icons/ti'
-import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io'
-import style from "./TripForm.module.css"
+import { useState, useEffect, useReducer } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { TiTick } from "react-icons/ti";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import style from "./TripForm.module.css";
 
 const EMPTY_STOP = {
-    "city": "",
-    "sublocation": "",
-    "imageUrl": "",
-    "description": ""
-}
+  city: "",
+  sublocation: "",
+  imageUrl: "",
+  description: "",
+};
 
-const getNewStop = () => ({...EMPTY_STOP})
+const getNewStop = () => ({ ...EMPTY_STOP });
 
 const EMPTY_TRIP = {
-  "country": "",
-  "stops": [{...EMPTY_STOP}]
-}
+  country: "",
+  stops: [{ ...EMPTY_STOP }],
+};
 
 const TripForm = ({ firstDraft, submit }) => {
+  const initialState = {
+    counter: 0,
+  };
+  const action = {
+    type: "decrease",
+    type: "increase",
+    type: "setCounter",
+  };
 
-    const [stops, setStops] = useState([])
-    const [country, setCountry] = useState(firstDraft?.country || '')
-    const [city, setCity] = useState(firstDraft?.city || '')
-    const [sublocation, setSublocation] = useState(firstDraft?.sublocation || '')
-    const [details, setDetails] = useState(firstDraft?.details || '')
-    const [imageUrl, setImageUrl] = useState("")
-    const [counter, setCounter] = useState(stops.length || 0)
+  function reducer(state, action) {
+    let newState;
+    switch (action.type) {
+      case "setCounter":
+        console.log("action.payload ", action.payload);
+        newState = { counter: action.payload };
+        break;
+      case "setCounterAfterRemove":
+        console.log("action.payload ", action.payload);
+        newState = { counter: action.payload };
+        break;
+      case "increase":
+        newState = { counter: state.counter + 1 };
+        break;
+      case "decrease":
+        newState = { counter: state.counter - 1 };
+        break;
+      default:
+        throw new Error();
+    }
+    return newState;
+  }
 
-    const addStop = () => {
-        if (!city || !sublocation || !details || !imageUrl) {
-            return;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [stops, setStops] = useState([]);
+  const [country, setCountry] = useState(firstDraft?.country || "");
+  const [city, setCity] = useState(firstDraft?.city || "");
+  const [sublocation, setSublocation] = useState(firstDraft?.sublocation || "");
+  const [details, setDetails] = useState(firstDraft?.details || "");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const addStop = () => {
+    if (!city || !sublocation || !details || !imageUrl) {
+      return;
+    }
+    setStops((prev) => [
+      ...prev,
+      {
+        city,
+        sublocation,
+        details,
+        imageUrl,
+      },
+    ]);
+    setCity("");
+    setSublocation("");
+    setDetails("");
+    setImageUrl("");
+  };
+  useEffect(() => {
+    if (stops.length) dispatch({ type: "setCounter", payload: stops.length });
+  }, [stops.length]);
+
+  const removeStop = () => {
+      //console.log(state.counter)
+      stops.splice(state.counter,1)
+      //console.log(stops)
+      setStops([...stops])
+      if(!stops.length) 
+      {
+          setCountry("")
         }
-        setStops(prev => ([...prev, {
-            city,
-            sublocation,
-            details,
-            imageUrl
-        }]))
-        setCity('')
-        setSublocation('')
-        setDetails('')
-        setImageUrl('')
-        
+      setCity("");
+      setSublocation("");
+      setDetails("");
+      setImageUrl("");
+  if(stops.length){
+      console.log("stops.length: " , stops.length)
+     //   dispatch({ type: "setCounterAfterRemove", payload: stops.length -1 })
+      }
+  };
+
+  const stepBackwards = () => {
+    if (state.counter) dispatch({ type: "decrease" });
+  };
+
+  const stepForwards = () => {
+    if (state.counter !== stops.length - 1) dispatch({ type: "increase" });
+  };
+
+  useEffect(() => {
+      console.log("state.counter: ",state.counter)
+    const prevStop = stops[state.counter];
+    if (prevStop) {
+      setCity(prevStop.city);
+      setSublocation(prevStop.sublocation);
+      setDetails(prevStop.details);
+      setImageUrl(prevStop.imageUrl);
     }
-    useEffect(()=> {   
-        if(stops.length) setCounter(stops.length -1)
-     }
-    ,[stops.length])
+  }, [state.counter]);
 
-    const removeStop = (index) => {
-        stops.filter(stop => stop[index] === index)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log({country,stops})
+    // if (!country || !city || !sublocation || !details || !imageUrl) {
+    //     return;
+    // }
+    // submit({country,stops})
+  };
 
-    const stepBackwards = () => {
-        setCounter(prev => prev ? prev - 1 : prev);
-    }
-
-    const stepForwards = () => {
-        setCounter(prev => prev === stops.length -1 ? prev  : prev +1);
-    }
-
-
-    useEffect(()=> {
-
-            console.log(counter);
-            const prevStop = stops[counter];
-            if (prevStop) {
-                setCity(prevStop.city)
-                setSublocation(prevStop.sublocation)
-                setDetails(prevStop.details)
-                setImageUrl(prevStop.imageUrl)
-            }
-    },[counter])
-    
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        // if (!country || !city || !sublocation || !details || !imageUrl) {
-        //     return;
-        // }
-        // submit({country,stops})
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input value={city} onChange={e => setCity(e.target.value)} />
-            <input value={country} onChange={e => setCountry(e.target.value)} />
-            <ImageUpload fileUrl={imageUrl} setFileUrl={setImageUrl} />
-            <input value={sublocation}  onChange={e => setSublocation(e.target.value)} />
-            <input value={details}  onChange={e => setDetails(e.target.value)} />
-            <button type="button" onClick={removeStop}><AiOutlineMinus /></button>
-            <button type="button" onClick={addStop}><AiOutlinePlus /></button>
-            <button type="submit"><TiTick /></button>
-            <button type="button" className={style.back} onClick={stepBackwards}><IoIosArrowBack/></button>
-            <button type="button" className={style.back} onClick={stepForwards}><IoIosArrowForward/></button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input value={city} onChange={(e) => setCity(e.target.value)} />
+      <input value={country} onChange={(e) => setCountry(e.target.value)} />
+      <ImageUpload fileUrl={imageUrl} setFileUrl={setImageUrl} />
+      <input
+        value={sublocation}
+        onChange={(e) => setSublocation(e.target.value)}
+      />
+      <input value={details} onChange={(e) => setDetails(e.target.value)} />
+      <button type="button" onClick={removeStop}>
+        <AiOutlineMinus />
+      </button>
+      <button type="button" onClick={addStop}>
+        <AiOutlinePlus />
+      </button>
+      <button type="submit">
+        <TiTick />
+      </button>
+      <button type="button" className={style.back} onClick={stepBackwards}>
+        <IoIosArrowBack />
+      </button>
+      <button type="button" className={style.back} onClick={stepForwards}>
+        <IoIosArrowForward />
+      </button>
+    </form>
+  );
+};
 
 export default TripForm;
